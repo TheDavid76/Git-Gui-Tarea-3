20ebc1ffeb63a9fc5d6022b2afd9a753ab632fa2
TheDavid76 <davidlucas190@gmail.com>
2017-05-16 09:29:05 +0200

    Creacion del contructor y de los getter y setter

--- Ingredientes.java
+++ Ingredientes.java
@@ -1,9 +1,34 @@
 
 public class Ingredientes {
 
-	private String Cantidad;
+	private String cantidad;
 	private String nombre;
 	
+
+	public Ingredientes(String cantidad, String nombre) {
+		this.cantidad = cantidad;
+		this.nombre = nombre;
+	}
+
+
+	public String getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 	
 	
 }
