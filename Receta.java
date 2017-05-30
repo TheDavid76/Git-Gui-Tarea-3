edca6cf1aea9ea7f0a56fd689b2f54f7da145c13
TheDavid76 <davidlucas190@gmail.com>
2017-05-30 08:46:13 +0200

    Añade a la clase Receta el método toString

--- Receta.java
+++ Receta.java
@@ -42,14 +42,43 @@ public class Receta {
 	}
 	
 	
-	
-	
 	public void addIngrediente(Ingredientes ingredientes) {	
-		
+		this.ingredientes.add(ingredientes);
 	}
 	
 	public void addPreparacion(String preparacion) {
-		
+		this.preparacion.add(preparacion);
 	}
-
+	
+	public String listarIngredientes() {
+		String lista="";
+		for(Ingredientes i:ingredientes) {
+			lista+=i.toString()+"\n";
+		}
+		return lista;
+	}
+	public String listarPreparacion() {
+		String lista="";
+		for(int i=0; i<preparacion.size(); i++) {
+			int paso = i+1;
+			lista+="Paso "+ paso + " : " +preparacion.get(i)+"\n";
+		}
+		return lista;
+	}
+	@Override
+	public String toString() {
+		return "Receta: " + nombre.toUpperCase() + "\n\nIngredientes\n" + listarIngredientes() + "\nPreparacion\n" + listarPreparacion();
+	}
+	 public static void main(String[] args) {
+		Ingredientes i = new Ingredientes(3, "kg", "tomates");
+		Ingredientes i2 = new Ingredientes(3, "kg", "macarrones");
+		Receta r = new Receta();
+		r.setNombre("Macarrones con tomate");
+		r.addIngrediente(i);
+		r.addIngrediente(i2);
+		r.addPreparacion("Pelar los tomates");
+		r.addPreparacion("Poner una olla con agua a hervir");
+		r.addPreparacion("Añadir los macarrones");
+		System.out.println(r.toString());
+	 }
 }
